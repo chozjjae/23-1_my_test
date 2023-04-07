11491d97567ab9016b83e02dad1fc041904a11a9
조재현 <90296055+chozjjae@users.noreply.github.com>
2023-04-07 21:06:10 +0900

    version_upgrade_04.07
    Co-Authored-By: chozjjae <90296055+chozjjae@users.noreply.github.com>

---
+++ Lecture_2023-1-main/ch2/2.1.4-1.js
@@ -0,0 +1,19 @@
+function add1(x, y) {
+  return x + y;
+}
+
+const add2 = (x, y) => {
+  return x + y;
+};
+
+const add3 = (x, y) => x + y;
+
+const add4 = (x, y) => (x + y);
+
+function not1(x) {
+  return !x;
+}
+var a = 2;
+console.log(not1(a));
+
+const not2 = x => !x;
