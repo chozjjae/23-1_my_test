b16175e262bbeb5df4fd266f35d55951a4979fa0
chozjjae <90296055+chozjjae@users.noreply.github.com>
2023-06-11 18:39:39 +0900

    home_test
    23.06.11 home/gittest

--- 0428/Lecture_2023-1-main/ch8/8.6/learn-mongoose/app.js
+++ 0428/Lecture_2023-1-main/ch8/8.6/learn-mongoose/app.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
   next(error);
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res, next) => { //사용량 제한 구현하기
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
   res.status(err.status || 500);
